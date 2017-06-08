0876f0f37ed586d12549055362d46aaa478c618f
Miguel Ferreira <feriguel@Miguels-Mac.local>
2017-06-08 02:30:28 +0100

    Added Spashscreen and joined all storyboards together

---
+++ Xplorer/Database/DBManager.c
@@ -0,0 +1,9 @@
+//
+//  DBManager.c
+//  Xplorer
+//
+//  Created by Miguel Ferreira on 08/06/2017.
+//  Copyright © 2017 something. All rights reserved.
+//
+
+#include "DBManager.h"
---
+++ Xplorer/Database/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  Xplorer
+//
+//  Created by Miguel Ferreira on 08/06/2017.
+//  Copyright © 2017 something. All rights reserved.
+//
+
+#ifndef DBManager_h
+#define DBManager_h
+
+
+#endif /* DBManager_h */
