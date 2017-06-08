64581858d077b37a3a93d0da6a74707c9493d490
Miguel Ferreira <feriguel@Miguels-Mac.local>
2017-06-08 14:44:41 +0100

    DBManager added

--- Xplorer/Database/DBManager.c
+++
@@ -1,9 +0,0 @@
-//
-//  DBManager.c
-//  Xplorer
-//
-//  Created by Miguel Ferreira on 08/06/2017.
-//  Copyright © 2017 something. All rights reserved.
-//
-
-#include "DBManager.h"
--- Xplorer/Database/DBManager.h
+++ Xplorer/Database/DBManager.h
@@ -6,8 +6,28 @@
 //  Copyright © 2017 something. All rights reserved.
 //
 
-#ifndef DBManager_h
-#define DBManager_h
+#import <sqlite3.h>
+#import <Foundation/Foundation.h>
 
+@interface DBManager : NSObject
 
-#endif /* DBManager_h */
+// ====================================================================================== //
+// Controler variables
+// ====================================================================================== //
+@property (nonatomic, strong) NSString *documentsDirectory;
+@property (nonatomic, strong) NSString *databaseFilename;
+@property (nonatomic, strong) NSMutableArray *arrResults;
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRows;
+@property (nonatomic) long long lastInsertedRowID;
+
+// ====================================================================================== //
+// Public methods
+// ====================================================================================== //
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+-(void)copyDatabaseIntoDocumentsDirectory;
+-(void)runQuery:(const char *)query isQueryExecutable:(BOOL)queryExecutable;
+-(NSArray *)loadDataFromDB:(NSString *)query;
+-(void)executeQuery:(NSString *)query;
+
+@end
